87bd7b76ebd7ceba04a62a5b28c5595c183b8168
Daniel-Abril68 <luisabril@colegioguanenta.edu.co>
2025-04-28 16:48:12 -0500

    se agregaron nuevos datos

--- modelo/conexion.php
+++ modelo/conexion.php
@@ -1,4 +1,30 @@
 <?php
+   // script para crear una conexion con la BD
+   
+   //parametros requeridos para la conexion la BD
 
+   // parametros BD local
+   DEFINE(`USER`, `root`); //crea la contaste USER con valor `root`
+   DEFINE(`PW`,  ``);
+   DEFINE(`HOST`,  `localhost`);
+   DEFINE(`BD`,  `Empresa`);
 
+   // parametros BD remota (infinityfree)
+
+   // conexion con la BD 
+   $conexion =mysqli_connect(HOST, USER, PW, BD);
+
+   // Establecer conjunto de caracteres para el hosting
+   mysqli_set_charser(sconexion, "utf8mb4");
+
+   //verificar la conexion con la BD
+   if(!$conexion)
+   {
+      die("la conexion con a BD fallo: " + mysql_error($conexion))
+      exit();
+   }
+   else
+   {
+      die("conexion a la BD exitosa!");
+   }
 ?>

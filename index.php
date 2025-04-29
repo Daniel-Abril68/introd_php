46eac12912063e2489117039cdb2af8ba5f5fafc
Daniel-Abril68 <luisabril@colegioguanenta.edu.co>
2025-04-29 09:49:12 -0500

    se agregaron nuevos codigos

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   <h1>pagina principal de sitio</h1> 
+   <h2>Luis Daniel Abril Fuentes</h2>
+   <!--formulario de inicio de sesion-->
+   <form action="">
+       <h2>Iniciar sesion</h2>
+       <label for="">E-mail:</label>
+       <input type="text" name="email" id="" requiered>
+       <br><br>
+       <label for="">password</label>
+       <input type="text" name="password" id="" required>
+       <br><br>
+       <button type="submit">ingresar</button>
+    </form>
+</body>
+</html>
--- modelo/conexion.php
+++ modelo/conexion.php
@@ -3,28 +3,32 @@
    
    //parametros requeridos para la conexion la BD
 
-   // parametros BD local
-   DEFINE(`USER`, `root`); //crea la contaste USER con valor `root`
-   DEFINE(`PW`,  ``);
-   DEFINE(`HOST`,  `localhost`);
-   DEFINE(`BD`,  `Empresa`);
+   // parametros DB local
+   DEFINE('USER', 'root'); //crea la contaste USER con valor `root`
+   DEFINE('PW',  '');
+   DEFINE('HOST', 'localhost');
+   DEFINE('BD',  'Empresa');
 
    // parametros BD remota (infinityfree)
+   /*DEFINE('USER', 'if0_38542091'); //crea la contaste USER con valor `root`
+   DEFINE('PW',  'daniel17082008');
+   DEFINE('HOST', 'sql100.infinityfree.com');
+   DEFINE('BD',  'if0_38542091_XXX');*/
 
    // conexion con la BD 
-   $conexion =mysqli_connect(HOST, USER, PW, BD);
+   $conexion = mysqli_connect(HOST, USER, PW, BD);
 
    // Establecer conjunto de caracteres para el hosting
-   mysqli_set_charser(sconexion, "utf8mb4");
+   mysqli_set_charset($conexion, "utf8mb4");
 
    //verificar la conexion con la BD
    if(!$conexion)
    {
-      die("la conexion con a BD fallo: " + mysql_error($conexion))
+      die("la conexion con la BD fallo: " + mysqli_error($conexion));
       exit();
    }
-   else
+   /*else
    {
       die("conexion a la BD exitosa!");
-   }
+   }*/
 ?>
